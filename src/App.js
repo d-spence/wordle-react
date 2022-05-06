20c5eff37698d8ac23e51a0b18eba946fa569cab
Dan Spencer <dspence180@gmail.com>
2022-05-05 20:16:59 -0500

    lesson-2 json-server

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { useState, useEffect } from "react";
+
+const dbUrl = 'http://localhost:3001/solutions';
+
 function App() {
+  const [solution, setSolution] = useState(null);
+
+  useEffect(() => {
+    fetch(dbUrl)
+      .then(res => res.json())
+      .then(json => {
+        const randomSolution = json[Math.floor(Math.random() * json.length) + 1];
+        setSolution(randomSolution.word);
+      });
+  }, [setSolution]);
+
   return (
     <div className="">
       <h1>Wordle</h1>
+      {solution && <div>Solution is: {solution}</div>}
     </div>
   );
 }
