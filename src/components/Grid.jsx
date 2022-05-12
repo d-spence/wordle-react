fc1747a83fb8047e9945f39b6326ad0ffd0198e1
Dan Spencer <dspence180@gmail.com>
2022-05-12 14:17:59 -0500

    lesson-10 showing current guess

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -4,6 +4,9 @@ const Grid = ({ currentGuess, guesses, turn }) => {
   return (
     <div>
       {guesses.map((guess, idx) => {
+        if (turn === idx) {
+          return <Row key={idx} currentGuess={currentGuess} />
+        }
         return <Row key={idx} guess={guess} />
       })}
     </div>
--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,11 +1,25 @@
-const Row = ({ guess }) => {
-
+const Row = ({ guess, currentGuess }) => {
   if (guess) {
     return (
       <div className="row past">
-        {guess.map((letter, idx) => {
-          return <div key={idx} className={letter.color}>{letter.key}</div>
-        })}
+        {guess.map((letter, idx) => (
+          <div key={idx} className={letter.color}>{letter.key}</div>
+        ))}
+      </div>
+    );
+  }
+
+  if (currentGuess) {
+    let letters = currentGuess.split('');
+
+    return (
+      <div className="row current">
+        {letters.map((letter, idx) => (
+          <div key={idx} className="filled">{letter}</div>
+        ))}
+        {[...Array(5 - letters.length)].map((value, idx) => (
+          <div key={idx}></div>
+        ))}
       </div>
     );
   }
